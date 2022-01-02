ac4add1b12dcff4c67390c12efb25fa37b2ed214
Kartofel-2834 <kanalori@mail.ru>
2022-01-02 11:19:33 +0300

    I add vue-router

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '@/views/Home.vue'
+import Registration from '@/views/Registration.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+
+  {
+    path: '/registration',
+    name: 'Registration',
+    component: Registration,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Registration.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hello Dear!</h1>
+</template>
