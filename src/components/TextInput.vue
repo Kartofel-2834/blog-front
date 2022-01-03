16053fd4a80224e076c8591c344645770b942aab
Kartofel-2834 <kanalori@mail.ru>
2022-01-03 12:08:56 +0300

    I wrote a part of registration input checkers

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="user_info_text_input_inner align">
-    <div class="input_title column align" :class="{ 'input_title_active': focused }">{{ title }}:</div>
+    <div
+      class="input_title column align"
+      :class="titleClasses"
+    >
+      {{ title }}:
+    </div>
+
     <input
       :type="type"
       class="user_info_text_input"
-      :class="css_classes"
+      :class="input_classes"
       @blur="blurCheck"
       @focus="focusedCheck"
       @input="input_listener"
+      :placeholder="placeholder"
+      :value="value"
     >
   </div>
 </template>
@@ -17,7 +25,10 @@
     props: {
       "type": { type: String, default: "text" },
       "title": { type: String, default: "Title" },
-      "css_classes": { type: Array, default: [] },
+      "value": { type: String, default: "" },
+      "placeholder": { type: String, default: "" },
+      "input_classes": { type: Array, default: [] },
+      "title_classes": { type: Array, default: [] },
       "blur_listener": { type: Function, default: ()=>{} },
       "focus_listener": { type: Function, default: ()=>{} },
       "input_listener": { type: Function, default: ()=>{} },
@@ -38,5 +49,19 @@
         this.blur_listener(e)
       },
     },
+
+    computed: {
+      titleClasses(){
+        let ans = new Set(this.title_classes)
+
+        if ( this.focused ){
+          ans.add('input_title_active')
+        } else {
+          ans.delete('input_title_active')
+        }
+
+        return Array.from(ans)
+      }
+    }
   }
 </script>
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="psevdo_body align center">
     <div class="reg_container column align">
-      <h1 class="title column align">Registration</h1>
+      <h1 class="title column align" :class="{ 'error_title': mainTitleError }">Registration</h1>
 
       <div class="inputs_inner column">
-        <text-input-form title="Mail" :input_listener="writeMailInData"></text-input-form>
+        <text-input-form
+          title="Mail"
+          :value="mail.text"
+          :input_listener="mailInputListener"
+          :title_classes="[ mail.error ? 'error_title' : '' ]"
+          :placeholder="mail.placeholder"
+        ></text-input-form>
 
-        <text-input-form title="Name" :input_listener="writeNameInData"></text-input-form>
+        <text-input-form
+          title="Name"
+          :value="name.text"
+          :input_listener="nameInputListener"
+          :title_classes="[ name.error ? 'error_title' : '' ]"
+          :placeholder="name.placeholder"
+        ></text-input-form>
 
-        <text-input-form title="Surname" :input_listener="writeSurnameInData"></text-input-form>
+        <text-input-form
+          title="Surname"
+          :value="surname.text"
+          :input_listener="surnameInputListener"
+          :title_classes="[ surname.error ? 'error_title' : '' ]"
+          :placeholder="surname.placeholder"
+        ></text-input-form>
 
         <div class="row password_input_inner">
           <text-input-form
             title="Password"
             :type="passwordShowed ? 'text' : 'password'"
-            :input_listener="writePasswordInData"
+            :input_listener="passwordInputListener"
           ></text-input-form>
 
           <div
@@ -25,7 +43,13 @@
         </div>
 
 
-        <text-input-form title="Tagname" :input_listener="writeTagnameInData"></text-input-form>
+        <text-input-form
+          title="Tagname"
+          :value="tagname.text"
+          :input_listener="tagnameInputListener"
+          :title_classes="[ tagname.error ? 'error_title' : '' ]"
+          :placeholder="tagname.placeholder"
+        ></text-input-form>
       </div>
 
       <div class="button submit_button" @click="submitButtonClickListener">Submit</div>
@@ -45,34 +69,67 @@
     data(){
       return {
         passwordShowed: false,
-        mail: '',
-        name: '',
-        surname: '',
-        password: '',
-        tagname: '',
+        mainTitleError: false,
+
+        mail: { text: '', error: false, placeholder: '' },
+        name: { text: '', error: false, placeholder: '' },
+        surname: { text: '', error: false, placeholder: '' },
+        password: { text: '', error: false, placeholder: '' },
+        tagname: { text: '', error: false, placeholder: '' },
       }
     },
 
     methods: {
       changePasswordInputType(){ this.passwordShowed = !this.passwordShowed },
 
-      writeMailInData(e){ this.mail = e.target.value },
-      writeNameInData(e){ this.name = e.target.value },
-      writeSurnameInData(e){ this.surname = e.target.value },
-      writePasswordInData(e){ this.password = e.target.value },
-      writeTagnameInData(e){ this.tagname = e.target.value },
+      inputListener(e, key){
+        this[key].text = e.target.value
+        this[key].error = false
+        this[key].placeholder = ""
+      },
+
+      mailInputListener(e){ this.inputListener(e, 'mail') },
+      nameInputListener(e){ this.inputListener(e, 'name') },
+      surnameInputListener(e){ this.inputListener(e, 'surname') },
+      passwordInputListener(e){ this.inputListener(e, 'password') },
+      tagnameInputListener(e){ this.inputListener(e, 'tagname') },
+
+      checkMail(){
+        const mailChecker = /[a-z]@[a-z]/
+
+        this.mail.error = !mailChecker.test(this.mail.text)
+        if ( this.mail.error ){
+          this.mail.placeholder = "Forbidden symbols!"
+          this.mail.text = ""
+        }
+      },
+
+      checkName(nametype){
+        const forbiddenSymbols = /^[^\%/\\&\?\,\'\;:!-+!@#\$\^*)(]{5,20}$/
+        let text = this[nametype].text
+
+        this[nametype].error = !forbiddenSymbols.test(text) || (text.length <= 5) || (text.length >= 20)
+
+        if ( this[nametype].error ){ this[nametype].text = '' }
+
+        if( text.length <= 5 ){ this[nametype].placeholder = 'Too short!'; return }
+
+        if( text.length >= 20 ){ this[nametype].placeholder = 'Too long!'; return }
+
+        if ( !forbiddenSymbols.test(text) ){ this[nametype].placeholder = 'Forbidden symbols!'; return }
+      },
+
+      checkNames(){
+        for (let nametype of ['name', 'surname', 'tagname']){
+          this.checkName(nametype)
+        }
+      },
 
       submitButtonClickListener(){
-        const mailChecker = /[a-z]@[a-z]/
-        const nameChecker = /^[^\%/\\&\?\,\'\;:!-+!@#\$\^*)(]{1,25}$/
+        this.checkMail()
+        this.checkNames()
 
-        if ( !mailChecker.test(this.mail) ){
-          //alert("майл не подходит")
-        }
-
-        if ( !nameChecker.test(this.name) ){
-          //alert("имя не подходит")
-        }
+        this.mainTitleError = this.mail.error || nameChecker.test(this.name.text)
       }
     },
   }
