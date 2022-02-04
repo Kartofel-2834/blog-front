83d77cc7dedcdf6bd3f01b3b5d0181eae16f14a3
Kartofel-2834 <kanalori@mail.ru>
2022-02-04 19:09:27 +0300

    I finished components styles separation

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -26,6 +26,8 @@
   </div>
 </template>
 
+<style src="@/assets/css/componentsStyles/postBlock.css"></style>
+
 <script type="text/javascript">
   import ImageBlock from "@/components/PostComponents/ImageBlock.vue"
   import GoneTimeParser from "@/components/PostComponents/GoneTimeParser.vue"
@@ -64,91 +66,3 @@
     }
   }
 </script>
-
-<style>
-  .post_block{
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-auto-rows: fit-content(100%);
-    row-gap: 2rem;
-
-    padding: 2rem;
-    font-size: 1rem;
-
-    background-color: white;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px 2px rgb(34 60 80 / 10%)
-  }
-
-  .post_top_nav, .post_info, .text_user_info{ font-size: inherit }
-
-  .post_top_nav{
-    width: 100%;
-    display: grid;
-    grid-template-columns: auto auto;
-    grid-template-rows: fit-content(300px);
-  }
-
-  .post_info{
-    font-weight: 500;
-    display: flex;
-    align-items: center;
-    justify-self: start;
-  }
-
-  .short_avatar{
-    width: 8vw !important;
-    height: 8vw !important;
-  }
-
-  .text_user_info{
-    display: grid;
-    grid-template-rows: fit-content(50%) fit-content(50%);
-    gap: 0.5rem;
-    margin-left: 1rem;
-  }
-
-  .name{ font-size: 0.9em }
-
-  .publication_date{
-    color: #959595;
-    font-size: 0.8em;
-  }
-
-  .post_text{ font-size: 1em }
-
-
-  @media all and (max-width: 700px){
-    .post_block{
-      padding: 1rem;
-      row-gap: 1rem;
-    }
-
-    .post_text{
-      font-size: 0.9em;
-    }
-  }
-
-  @media all and (max-width: 950px){
-    .short_avatar{
-      width: 55px !important;
-      height: 55px !important;
-    }
-  }
-
-  @media all and (max-width: 1500px) and (min-width: 800px) {
-    .post_block{ font-size: 1.2rem }
-  }
-
-  @media all and (min-width: 1500px) {
-    .post_block{ font-size: 1.5rem }
-  }
-
-  @media all and (min-width: 1800px) {
-    .post_block{ font-size: 1.8rem }
-  }
-
-  @media all and (min-width: 2200px) {
-    .post_block{ font-size: 2rem }
-  }
-</style>
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -23,6 +23,8 @@
   </div>
 </template>
 
+<style src="@/assets/css/componentsStyles/userInfo.css"></style>
+
 <script>
   export default {
     props: { "user": { type: Object, default: {} } },
@@ -30,97 +32,3 @@
     emits: ['postCreationFieldOpen']
   }
 </script>
-
-
-<style>
-  .main_user_info{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    font-size: 1rem;
-  }
-
-  .avatar{
-    width: 100%;
-    height: auto;
-  }
-
-  .user_name{
-    font-weight: 600;
-    margin-top: 1rem;
-    font-size: 1.3em;
-  }
-
-  .user_tag{
-    font-size: 1em;
-    margin-top: 0.5rem;
-    color:grey;
-  }
-
-  .stat_row{
-    display: flex;
-    font-size: 1rem;
-    margin-top: 1.5rem;
-  }
-
-  .stat{
-    margin-left: 2rem;
-    font-size: inherit;
-  }
-
-  .stat:first-child{ margin-left: 0 }
-
-  .stat_num{
-    font-size: 1.5em;
-    font-weight: 600;
-  }
-
-  .stat_name{
-    font-size: 0.7em;
-    text-transform: uppercase;
-    font-weight: 500;
-  }
-
-  .main_page_button{
-    font-size: 1rem;
-    margin: 2rem auto;
-  }
-
-  @media all and (max-width: 700px){
-    .main_user_info, .stat_row{ font-size: 0.9rem }
-
-    .main_page_button{
-      font-size: 1.1rem;
-      padding: 1rem 2rem;
-      margin: 1.5rem auto;
-    }
-  }
-
-  @media all and (max-width: 950px){
-     .main_user_info, .stat_row{ font-size: 1.1rem }
-  }
-
-  @media all and (min-width: 1400px){
-    .main_user_info{ font-size: 1.3rem }
-
-    .main_page_button{
-      font-size: 1.4rem;
-      padding: 1.2rem 2.7rem;
-    }
-  }
-
-  @media all and (min-width: 1800px){
-    .main_user_info{ font-size: 1.5rem }
-  }
-
-  @media all and (min-width: 2200px){
-    .main_user_info, .stat_row{ font-size: 2rem }
-
-    .main_page_button{
-      font-size: 2rem;
-      padding: 1.5rem 4rem;
-      margin: 4rem auto;
-    }
-  }
-</style>
